7d276ea16c076adef3925452f0cff82c72bcf633
Brendon <brendon.nhachi@mixtelematics.com>
2026-02-20 13:14:36 +0200

    Initial STM32 driver setup

---
+++ drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32l476xx.h
+ *
+ *  Created on: Feb 11, 2026
+ *      Author: BrendonN
+ */
+
+#ifndef INC_STM32L476XX_H_
+#define INC_STM32L476XX_H_
+
+
+
+#endif /* INC_STM32L476XX_H_ */
---
+++ drivers/stm32l476xx_drivers/gpio/drivers/Src/stm32l476xx.c
@@ -0,0 +1,15 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Brendon N
+ * @brief          : Main program body
+ ******************************************************************************
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+	for(;;);
+}
