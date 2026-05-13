d5aa0e8f4dafe26da159c5256dc9f98a5ab651cf
Brendon <brendon.nhachi@mixtelematics.com>
2026-05-13 16:02:38 +0200

    [Added APB1 peripheral base addresses]

--- drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
+++ drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
@@ -15,7 +15,6 @@
 
 
 /*  AHBx & APBx Bus Peripheral base Addresses  */
-
 #define PERIPH_BASE                                    0x40000000U
 #define APB1PERIPH_BASE                                PERIPH_BASE
 #define APB2PERIPH_BASE                                0x40010000U
@@ -26,7 +25,6 @@
  * Base Addresses for AHB2 (GPIO) bus
  * Todo: Complete for peripherals on bus
  *  */
-
 #define GPIOA_BASEADDR                                 (AHB2PERIPH_BASE + 0x0000)
 #define GPIOB_BASEADDR                                 (AHB2PERIPH_BASE + 0x0400)
 #define GPIOC_BASEADDR                                 (AHB2PERIPH_BASE + 0x0800)
@@ -35,5 +33,20 @@
 #define GPIOF_BASEADDR                                 (AHB2PERIPH_BASE + 0x1400)
 #define GPIOG_BASEADDR                                 (AHB2PERIPH_BASE + 0x1800)
 #define GPIOH_BASEADDR                                 (AHB2PERIPH_BASE + 0x1C00)
+
+/*
+ * Base Addresses for APB1 (I2c, UUART, USART, CAN1, SPI) bus
+ * Todo: Complete for peripherals on bus
+ *  */
+#define CAN1_BASEADDR                                  (APB1PERIPH_BASE + 0x6400)
+#define I2C1_BASEADDR                                  (APB1PERIPH_BASE + 0x5400)
+#define I2C2_BASEADDR                                  (APB1PERIPH_BASE + 0x5800)
+#define I2C3_BASEADDR                                  (APB1PERIPH_BASE + 0x5C00)   
+#define USART2_BASEADDR                                (APB1PERIPH_BASE + 0x4400)
+#define USART3_BASEADDR                                (APB1PERIPH_BASE + 0x4800)
+#define UART4_BASEADDR                                 (APB1PERIPH_BASE + 0x4C00)
+#define UART5_BASEADDR                                 (APB1PERIPH_BASE + 0x5000)
+#define SPI2_BASEADDR                                  (APB1PERIPH_BASE + 0x3800)
+#define SPI3_BASEADDR                                  (APB1PERIPH_BASE + 0x3C00)
 
 #endif /* INC_STM32L476XX_H_ */
