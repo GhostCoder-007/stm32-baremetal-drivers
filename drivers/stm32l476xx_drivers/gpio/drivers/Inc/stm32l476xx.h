64a8d22ad60d7f5f9e24310e968b77f918db5b4f
Brendon <brendon.nhachi@mixtelematics.com>
2026-05-13 15:48:13 +0200

    [heade] [added gpio base addresses]

--- drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
+++ drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
@@ -22,4 +22,18 @@
 #define AHB1PERIPH_BASE                                0x40020000U
 #define AHB2PERIPH_BASE                                0x48000000U
 
+/*
+ * Base Addresses for AHB2 (GPIO) bus
+ * Todo: Complete for peripherals on bus
+ *  */
+
+#define GPIOA_BASEADDR                                 (AHB2PERIPH_BASE + 0x0000)
+#define GPIOB_BASEADDR                                 (AHB2PERIPH_BASE + 0x0400)
+#define GPIOC_BASEADDR                                 (AHB2PERIPH_BASE + 0x0800)
+#define GPIOD_BASEADDR                                 (AHB2PERIPH_BASE + 0x0C00)
+#define GPIOE_BASEADDR                                 (AHB2PERIPH_BASE + 0x1000)
+#define GPIOF_BASEADDR                                 (AHB2PERIPH_BASE + 0x1400)
+#define GPIOG_BASEADDR                                 (AHB2PERIPH_BASE + 0x1800)
+#define GPIOH_BASEADDR                                 (AHB2PERIPH_BASE + 0x1C00)
+
 #endif /* INC_STM32L476XX_H_ */
