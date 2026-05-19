8264cb5b962f8c08b68acbff10fe4db1b786136c
Brendon <brendon.nhachi@mixtelematics.com>
2026-05-19 16:12:41 +0200

    [bn001] [added volatile + int header]

--- drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
+++ drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx.h
@@ -8,11 +8,14 @@
 #ifndef INC_STM32L476XX_H_
 #define INC_STM32L476XX_H_
 
+#include <stdint.h> // standard header for integer types (e.g., uint32_t)
+
 #define FLASH_BASEADDR                                 0x8000000U
 #define SRAM1_BASEADDR                                 0x20000000U
 #define SRAM2_BASEADDR                                 0x10000000U
 #define ROM_BASEADDR                                   0x1FFF0000U // System memory
 
+#define __vo volatile
 
 /*  AHBx & APBx Bus Peripheral base Addresses  */
 #define PERIPH_BASE                                    0x40000000U
@@ -69,17 +72,17 @@
  typedef struct 
  {
 
-    uint32_t MODER;    /*|< GPIO port mode register,                           Address offset: 0x00 */
-    uint32_t OTYPER;   /*|< GPIO port output type register,                    Address offset: 0x04 */
-    uint32_t OSPEEDR;  /*|< GPIO port output speed register,                   Address offset: 0x08 */
-    uint32_t PUPDR;    /*|< GPIO port pull-up/pull-down register,              Address offset: 0x0C */
-    uint32_t IDR;      /*|< GPIO port input data register,                     Address offset: 0x10 */
-    uint32_t ODR;      /*|< GPIO port output data register,                    Address offset: 0x14 */
-    uint32_t BSRR;     /*|< GPIO port bit set/reset register,                  Address offset: 0x18 */
-    uint32_t LCKR;     /*|< GPIO port configuration lock register,             Address offset: 0x1C */
-    uint32_t AFR[2];   /*|< GPIO alternate function low/high register,         Address offset: 0x20-0x24 */
-    uint32_t BRR;      /*|< GPIO port bit reset register,                      Address offset: 0x28 */
-    uint32_t ASCR;     /*|< GPIO port analog switch control register,          Address offset: 0x2C */
+    __vo uint32_t MODER;    /*|< GPIO port mode register,                             Address offset: 0x00 */
+    __vo uint32_t OTYPER;   /*|< GPIO port output type register,                      Address offset: 0x04 */
+    __vo uint32_t OSPEEDR;  /*|< GPIO port output speed register,                     Address offset: 0x08 */
+    __vo uint32_t PUPDR;    /*|< GPIO port pull-up/pull-down register,                Address offset: 0x0C */
+    __vo uint32_t IDR;      /*|< GPIO port input data register,                       Address offset: 0x10 */
+    __vo uint32_t ODR;      /*|< GPIO port output data register,                      Address offset: 0x14 */
+    __vo uint32_t BSRR;     /*|< GPIO port bit set/reset register,                    Address offset: 0x18 */
+    __vo uint32_t LCKR;     /*|< GPIO port configuration lock register,               Address offset: 0x1C */
+    __vo uint32_t AFR[2];   /*|< GPIO alternate function low (0) / high (1) register, Address offset: 0x20-0x24 */
+    __vo uint32_t BRR;      /*|< GPIO port bit reset register,                        Address offset: 0x28 */
+    __vo uint32_t ASCR;     /*|< GPIO port analog switch control register,            Address offset: 0x2C */
 
  } GPIO_regDef_t;
 
