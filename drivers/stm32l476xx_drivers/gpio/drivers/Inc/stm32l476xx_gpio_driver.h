95bd8e486b1373dc9e37ffcef9a15e93b8708af0
Brendon <brendon.nhachi@mixtelematics.com>
2026-06-23 12:09:17 +0200

    [BN_002] [add gpio handle struct + config struct]

--- drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx_gpio_driver.h
+++ drivers/stm32l476xx_drivers/gpio/drivers/Inc/stm32l476xx_gpio_driver.h
@@ -10,4 +10,21 @@
 
 #include "stm32l476xx.h"
 
+typedef struct
+{
+    uint8_t GPIO_PinNumber; /*!< possible values from @GPIO_PIN_NUMBERS */
+    uint8_t GPIO_PinMode; /*!< possible values from @GPIO_PIN_MODES */
+    uint8_t GPIO_PinSpeed; /*!< possible values from @GPIO_PIN_SPEED */
+    uint8_t GPIO_PinPuPdControl; /*!< possible values from @GPIO_PIN_PUPD */
+    uint8_t GPIO_PinOPType; /*!< possible values from @GPIO_OP_TYPE */
+    uint8_t GPIO_PinAltFunMode; /*!< possible values from @GPIO_ALT_FUNC_MODES */
+}GPIO_PinConfig_t;
+
+typedef struct
+{
+    GPIO_regDef_t*pGPIOx; /*!< This holds the base address of the GPIO port to which the pin belongs */
+    GPIO_PinConfig_t*pGPIOConfig; /*!< This holds the GPIO pin configuration settings */
+
+}GPIO_HANDLE_t;
+
 #endif /* INC_STM32L476XX_GPIO_DRIVER_H_ */
